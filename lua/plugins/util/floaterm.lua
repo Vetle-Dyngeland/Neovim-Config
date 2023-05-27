181183798635889e8111dc0a0696589a864c2f37
Vetle Dyngeland <vetle.dyngeland@gmail.com>
2023-05-27 15:29:28 +0200

    better floaterm arguments
    made it easier to add arguments to floaterm

--- lua/plugins/util/floaterm.lua
+++ lua/plugins/util/floaterm.lua
@@ -1,3 +1,29 @@
+local function split_string(inputstr, sep)
+    if sep == nil then
+        sep = "%s"
+    end
+
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+        table.insert(t, str)
+    end
+    return t
+end
+
+local function remove_occurences(inputstr, char)
+    return inputstr:gsub(char,"")
+end
+
+local function format_args(inputstr)
+    inputstr = remove_occurences(inputstr, "-")
+    local args = split_string(inputstr, " ")
+    local retstr = ""
+    for i = 1, #args do
+        retstr = retstr .. "--" .. args[i] .. " "
+    end
+    return retstr
+end
+
 return {
     {
         "voldikss/vim-floaterm",
@@ -10,7 +36,7 @@ return {
 
             map("n", "<leader>ta", function()
                 local input = vim.fn.input("Floaterm arguments > ")
-                vim.cmd([[FloatermNew]] .. input)
+                vim.cmd([[FloatermNew]] .. format_args(input))
             end, { desc = "Open new floaterm with the given arguments" })
 
             map("n", "<leader>tt", "<cmd>FloatermToggle<cr>", { desc = "Toggle floaterm" })
@@ -24,11 +50,11 @@ return {
 
             map("n", "<leader>tu", function()
                 local input = vim.fn.input("New floaterm arguments > ")
-                vim.cmd([[FloatermUpdate]] .. input)
+                vim.cmd([[FloatermUpdate]] .. format_args(input))
             end, { desc = "Update current floaterm with the given arguments" })
             map("t", "<leader>tu", function()
                 local input = vim.fn.input("New floaterm arguments > ")
-                vim.cmd([[FloatermUpdate]] .. input)
+                vim.cmd([[FloatermUpdate]] .. format_args(input))
             end, { desc = "Update current floaterm with the given arguments" })
         end
     }
